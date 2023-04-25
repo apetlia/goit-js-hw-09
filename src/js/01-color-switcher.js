193574eafe01b1cbcfb572e8530a8ac8d87664cb
Artem Petlia <pak@servers.com>
2023-04-25 19:33:28 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
 
+refs.buttonStart.addEventListener('click', onButtonStartClick);
+refs.buttonStop.addEventListener('click', onButtonStopClick);
+
+buttonToggleDisable(refs.buttonStop);
+
+let timerId = null;
+
+function onButtonStartClick() {
+  changeBodyBgColor();
+  timerId = setInterval(changeBodyBgColor, 1000);
+  buttonToggleDisable(refs.buttonStart);
+  buttonToggleDisable(refs.buttonStop);
+}
+
+function onButtonStopClick() {
+  clearInterval(timerId);
+  buttonToggleDisable(refs.buttonStart);
+  buttonToggleDisable(refs.buttonStop);
+}
+
+function changeBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function buttonToggleDisable(button) {
+  button.disabled = button.disabled ? false : true;
+}
